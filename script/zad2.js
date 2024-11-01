9f79e287ba90aaa7f297aec874118314721901b1
Crown22123 <156973173+Crown22123@users.noreply.github.com>
2024-11-02 04:08:08 +0700

    победа + проверка на дурака

--- script/zad2.js
+++ script/zad2.js
@@ -1,6 +1,11 @@
 document.getElementById("btn2").onclick = task2;
 function task2(){
     let chislo = Number(document.getElementById("chislo").value);
+    let otvet;
+    if (chislo <1000){
+         otvet='Неправильное число'
+    }
+    else{
     let thousands = Math.floor(chislo % 10);
     chislo=Math.floor(chislo / 10);
     let hundreds = Math.floor(chislo % 10);
@@ -8,8 +13,8 @@ function task2(){
     let tens = Math.floor(chislo % 10);
     chislo=Math.floor(chislo / 10);
     let units = chislo % 10;
-    let reversed_chislo = thousands * 1000 + hundreds  * 100 + tens* 10 + units;
-    
-    document.getElementById('output_task2').innerHTML = `перевернутое число: ${reversed_chislo}`; 
+    otvet = thousands * 1000 + hundreds  * 100 + tens* 10 + units;
+}
+    document.getElementById('output_task2').innerHTML = `перевернутое число: ${otvet}`; 
     return;
 }
