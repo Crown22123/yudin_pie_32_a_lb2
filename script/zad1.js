6d8beaf56bff8223d157b7237bd0360e18d4a602
Crown22123 <156973173+Crown22123@users.noreply.github.com>
2024-11-02 01:48:48 +0700

    ура
    новые указания к лабе

--- script/zad1.js
+++ script/zad1.js
@@ -1,8 +1,10 @@
-alert("№1");
-let m = Number(prompt('Введите количество побед '));
-let n = Number(prompt('Введите количество поражений '));
-let k = Number(prompt('Введите количество ничей '));
+document.getElementById("btn1").onclick = task1;
+function task1(){
+    let win = Number(document.getElementById("win").value);
+    let defeat = Number(document.getElementById("defeat").value);
+    let draw = Number(document.getElementById("draw").value);
 
-let summa_score = 0;
-summa_score = m*2+k*1;
-alert(summa_score);
+    let sum = win*2+draw*1
+    document.getElementById('output_task1').innerHTML = `Сумма очков равна: ${sum}`; 
+    return;
+}
--- script/zad2.js
+++ script/zad2.js
@@ -1,12 +1,15 @@
-alert("№2");
-let chislo = Number(prompt('Введите четырехзначное число'));
-let thousands = Math.floor(chislo % 10);
-chislo=Math.floor(chislo / 10);
-let hundreds = Math.floor(chislo % 10);
-chislo=Math.floor(chislo / 10);
-let tens = Math.floor(chislo % 10);
-chislo=Math.floor(chislo / 10);
-let units = chislo % 10;
-let reversed_chislo = thousands * 1000 + hundreds  * 100 + tens* 10 + units;
-
-alert (reversed_chislo);
+document.getElementById("btn2").onclick = task2;
+function task2(){
+    let chislo = Number(document.getElementById("chislo").value);
+    let thousands = Math.floor(chislo % 10);
+    chislo=Math.floor(chislo / 10);
+    let hundreds = Math.floor(chislo % 10);
+    chislo=Math.floor(chislo / 10);
+    let tens = Math.floor(chislo % 10);
+    chislo=Math.floor(chislo / 10);
+    let units = chislo % 10;
+    let reversed_chislo = thousands * 1000 + hundreds  * 100 + tens* 10 + units;
+    
+    document.getElementById('output_task2').innerHTML = `перевернутое число: ${reversed_chislo}`; 
+    return;
+}
